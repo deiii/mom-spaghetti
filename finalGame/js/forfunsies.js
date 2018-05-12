73ce036cc929e40eb83d934a26f6b92d01f0c989
deiii <38969957+deiii@users.noreply.github.com>
2018-05-12 14:18:56 -0700

    Added first prototype for Drumpf

--- finalGame/js/forfunsies.js
+++ finalGame/js/forfunsies.js
@@ -101,20 +101,160 @@ function nextWord() {
 
 
  	// ----------------------------------------------------------------------------------------------------------------------------------------Mini Game 1
-// define GamePlay state and methods
+var thots;
+// Mini Game 1
 var miniGameA = function(game) {};
 miniGameA.prototype = {
 	preload: function() {
+		game.load.atlas('trumpthots', 'assets/img/trumpthots.png', 'assets/img/trumpthots.json', Phaser.Loader.TEXTURE_ATLAS_JSON_HASH);
+		game.load.image('mouth', 'assets/img/block.png');
+		game.load.image('trump', 'assets/img/trumpface.png');
+
+		game.load.audio('byebing', ['assets/audio/byebing.mp3', 'assets/audio/byebing.ogg']);
+		game.load.audio('hello', ['assets/audio/hello.mp3', 'assets/audio/hello.ogg']);
+		game.load.audio('kachingka', ['assets/audio/kachingka.mp3', 'assets/audio/kachingka.ogg']);
+		game.load.audio('moan', ['assets/audio/moan.mp3', 'assets/audio/moan.ogg']);
+		game.load.audio('nyoom', ['assets/audio/nyoom.mp3', 'assets/audio/nyoom.ogg']);
+		game.load.audio('woh', ['assets/audio/woh.mp3', 'assets/audio/woh.ogg']);
 	},
 	create: function() {
 		console.log('mini game');
-		game.stage.backgroundColor = "#80C67F";
+		game.stage.backgroundColor = "#852109";
+
+		mouth = game.add.sprite(450,450,'mouth');
+		game.physics.enable(mouth, Phaser.Physics.ARCADE);
+		mouth.body.immovable = true;
+		mouth.body.allowGravity = false;
+
+
 		// --------------------------------------------------
 		// Instructions for state switching
 		// --------------------------------------------------
-		instruction2 = game.add.text(300,50,'Press SPACE to go to main menu\nPress ESC for game over', { font: '32px Serif'});
+
+		game.physics.startSystem(Phaser.Physics.ARCADE);
+		// game.physics.arcade.gravity.y = 50;  
+
+		//-------------------------------------------
+		// Create a group for words and 
+		// enable physics on it.
+		//-------------------------------------------
+		thots = game.add.group();
+		thots.inputEnableChildren = true;
+		thots.enableBody = true;
+		thots.physicsBodyType = Phaser.Physics.ARCADE;
+
+
+		//---------------------------------------
+		// Load in words from atlas
+		//-------------------------------------------
+		for(i = 0; i < 5; i++)
+		{
+			// game.rnd.integerInRange(20, 650)
+			var word1 = thots.create(game.world.randomX, game.rnd.integerInRange(50, 400),'trumpthots', 'dangerous');
+			var word2 = thots.create(game.world.randomX, game.rnd.integerInRange(50, 400),'trumpthots','tremendous');
+			var word3 = thots.create(game.world.randomX, game.rnd.integerInRange(50, 400),'trumpthots', 'tough');
+			var word4 = thots.create(game.world.randomX, game.rnd.integerInRange(50, 400),'trumpthots', 'stupid');
+			var word5 = thots.create(game.world.randomX, game.rnd.integerInRange(50, 400),'trumpthots', 'terrific');
+			var word6 = thots.create(game.world.randomX, game.rnd.integerInRange(50, 400),'trumpthots', 'smart');
+			var word7 = thots.create(game.world.randomX, game.rnd.integerInRange(50, 400),'trumpthots', 'loser');
+			var word8 = thots.create(game.world.randomX, game.rnd.integerInRange(50, 400),'trumpthots', 'weak');
+
+			word1.inputEnabled = true;
+			word1.input.enableDrag(true);
+
+			word1.body.velocity.x = 200;
+			word1.body.acceleration.x = 100;
+
+			word1.body.bounce.x = 1;
+			word1.body.bounce.y = 2.5;
+
+			word2.inputEnabled = true;
+			word2.input.enableDrag(true);
+
+			word2.body.velocity.x = 200;
+			word2.body.acceleration.x = 100;
+
+			word2.body.bounce.x = 1;
+			word2.body.bounce.y = 2.5;
+			
+			word3.inputEnabled = true;
+			word3.input.enableDrag(true);
+
+			word3.body.velocity.x = 200;
+			word3.body.acceleration.x = 100;
+			
+			word3.body.bounce.x = 1;
+			word3.body.bounce.y = 2.5;
+			
+			word4.inputEnabled = true;
+			word4.input.enableDrag(true);
+
+			word4.body.velocity.x = 200;
+			word4.body.acceleration.x = 100;
+
+			word4.body.bounce.x = 1;
+			word4.body.bounce.y = 2.5;
+			
+			word5.inputEnabled = true;
+			word5.input.enableDrag(true);
+
+			word5.body.velocity.x = 200;
+			word5.body.acceleration.x = 100;
+
+			word5.body.bounce.x = 1;
+			word5.body.bounce.y = 2.5;
+
+			word6.inputEnabled = true;
+			word6.input.enableDrag(true);
+
+			word6.body.velocity.x = 200;
+			word6.body.acceleration.x = 100;
+
+			word6.body.bounce.x = 1;
+			word6.body.bounce.y = 2.5;
+
+			word7.inputEnabled = true;
+			word7.input.enableDrag(true);
+
+			word7.body.velocity.x = 200;
+			word7.body.acceleration.x = 100;
+
+			word7.body.bounce.x = 1;
+			word7.body.bounce.y = 2.5;
+			
+			word8.inputEnabled = true;
+			word8.input.enableDrag(true);
+
+			word8.body.velocity.x = 200;
+			word8.body.acceleration.x = 100;
+
+			word8.body.bounce.x = 1;
+			word8.body.bounce.y = 2.5;
+		}
+			// "listen" to the group events
+			thots.onChildInputOver.add(this.onOver, this);
+			thots.onChildInputOut.add(this.onOut, this);
+
+			trump = game.add.sprite(0, 0,'trump');
+			instruction2 = game.add.text(20,500,'Press SPACE to go to main menu\nPress ESC for game over', { font: '32px Serif', fill: '#D5D5D5'});
 	},
+
+	onOver: function(word) {
+		word.tint = 0x00ff00;
+	},
+
+	onOut: function(word) {
+		word.tint = 0xffffff;
+	},
+
 	update: function() {
+		// --------------------------------------------------
+		// Make all words wrap
+		// --------------------------------------------------
+		game.world.wrapAll(thots);
+
+		game.physics.arcade.collide(thots,mouth,this.begoneThot,null,this);
+		// game.physics.arcade.collide(thots,moses);
 		// --------------------------------------------------
 		// Press SPACE for main menu
 		// Press ESC for game over
@@ -125,6 +265,24 @@ miniGameA.prototype = {
 		if(game.input.keyboard.isDown(Phaser.Keyboard.ESC)) {
 			game.state.start('GameOver');
 		}
+	},
+
+	begoneThot: function(mouth, word) {
+		// Kill the word sprite
+		word.kill();
+		// List of random sounds
+		byebing = game.add.audio('byebing');
+		hello = game.add.audio('hello');
+		kachingka = game.add.audio('kachingka');
+		moan = game.add.audio('moan');
+		nyoom = game.add.audio('nyoom');
+		woh = game.add.audio('woh');
+		// Put sounds into array
+		grunts = [byebing, hello, kachingka, moan, nyoom, woh];
+		// Get a random item from the array
+		randomGrunt = Phaser.ArrayUtils.getRandomItem(grunts, 0, grunts.length);
+		// Play that random item
+		randomGrunt.play();
 	}
 }
 	// ----------------------------------------------------------------------------------------------------------------------------------------Mini Game 2
