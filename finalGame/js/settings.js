a95629b8b80804b090ec9549aa5e5fe49c903726
aarancib <38925708+aarancib@users.noreply.github.com>
2018-06-07 12:38:06 -0700

    Update Settings.js
    States fixed

--- finalGame/js/settings.js
+++ finalGame/js/settings.js
@@ -1,23 +1,26 @@
-	// settings state --------------------------------------------------------------------
+// settings state --------------------------------------------------------------------
 var settings = function(game) {};
 	settings.prototype = {
 		preload: function() {
 			//load in "back" button
 			game.load.image('back', 'assets/img/backButton.png');
 		},
-		backMenu: function() {
-		//change to loadingMenu
-		game.state.start('loadingMenu');
-	},
+		
 		create: function() {
 			music.pause();
-			game.stage.backgroundColor = palette.grey;
+			game.stage.backgroundColor = '#ffffff';
 			console.log('settings');
 			var back = game.add.sprite(100, 500, 'back');
 			back.inputEnabled = true;
-			back.events.onInputDown.add(backMenu, this);
-			game.add.text(125, 505, "Back", textStyle);
+			back.events.onInputDown.add(this.backMenu, this);
+			game.add.text(125, 505, "Back", {font: 'Orbitron',
+				fontSize: 32,
+				fill: '#000000'});
 		},
 		update: function() {
+		},
+		backMenu: function() {
+		//change to loadingMenu
+		game.state.start('loadingMenu');
 		}
 	}
