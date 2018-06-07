958177f32e4c0febbb0f0070ec348497db1d505a
aarancib <38925708+aarancib@users.noreply.github.com>
2018-06-07 12:38:47 -0700

    Update triggers.js
    Fixed states switching

--- finalGame/js/triggers.js
+++ finalGame/js/triggers.js
@@ -8,21 +8,25 @@ var backMenu;
 		},
 		create: function() {
 			music.pause();
-			game.stage.backgroundColor = palette.grey;
+			game.stage.backgroundColor = '#ffffff';
 			console.log('triggers');
 			// text about triggers
 	   		var trig = "This game contains some political\ncommmentary please be advised.\n\nGameplay may also cause motion\nsickness and anxiety...\n\nPlay at your own discretion.";
-			game.add.text(100, 100, trig, textStyle);
+			game.add.text(100, 100, trig, {font: 'Orbitron',
+				fontSize: 32,
+				fill: '#000000'});
 		
 			var back = game.add.sprite(100, 500, 'back');
 			back.inputEnabled = true;
-			back.events.onInputDown.add(backMenu, this);
-			game.add.text(125, 505, "Back", textStyle);
+			back.events.onInputDown.add(this.backMenu, this);
+			game.add.text(125, 505, "Back", {font: 'Orbitron',
+											fontSize: 32,
+											fill: '#000000'});
 		},
 		update: function() {
 		},
 		backMenu: function() {
-		//change to loadingMenu
-		game.state.start('loadingMenu');
-	}
+			//change to loadingMenu
+			game.state.start('loadingMenu');
+		}
 	}
