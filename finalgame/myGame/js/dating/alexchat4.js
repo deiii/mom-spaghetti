e949a8d39874fefcd06c68a5c8438a1a8106dbe8
deiii <38969957+deiii@users.noreply.github.com>
2018-06-11 00:10:53 -0700

    Add files via upload

---
+++ finalgame/myGame/js/dating/alexchat4.js
@@ -0,0 +1,194 @@
+var PIECE_WIDTH = 180,
+    PIECE_HEIGHT = 210,
+    BOARD_COLS,
+    BOARD_ROWS;
+
+var piecesGroup,
+    piecesAmount,
+    shuffledIndexArray = [];
+
+var incchat = 0;
+
+var alexchat4 = function(game) {};
+alexchat4.prototype = {
+    preload: function() {
+        game.load.spritesheet("sprite", "assets/img/chat/ball.png", PIECE_WIDTH, PIECE_HEIGHT);
+        game.load.atlas('hearts', 'assets/img/chat/hearts.png', 'assets/img/chat/hearts.json', Phaser.Loader.TEXTURE_ATLAS_JSON_HASH);
+    },
+
+    create: function(){
+        this.camera.flash('#000000', 700);
+
+        this.prepareBoard();
+    },
+    update: function() {
+        if(game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) {
+            game.state.start('phase4');
+            game.sound.stopAll();
+            if (((localStorage.getItem("incb4") >= 1) && 
+                (localStorage.getItem("incsh4") >= 1) && 
+                (localStorage.getItem("inctr4") >= 1)))
+            {
+                game.sound.stopAll();
+                game.state.start('phase5');
+            } 
+        }
+    },
+    prepareBoard: function(){
+        var piecesIndex = 0,
+            i, j,
+            piece;
+
+        BOARD_COLS = Math.floor(game.world.width / PIECE_WIDTH);
+        BOARD_ROWS = Math.floor(game.world.height / PIECE_HEIGHT);
+        
+        piecesAmount = BOARD_COLS * BOARD_ROWS;
+
+        shuffledIndexArray = this.createShuffledIndexArray();
+
+        piecesGroup = game.add.group();
+        
+        for (i = 0; i < BOARD_ROWS; i++)
+        {
+            for (j = 0; j < BOARD_COLS; j++)
+            {
+                if (shuffledIndexArray[piecesIndex]) {
+                    piece = piecesGroup.create(j * PIECE_WIDTH, i * PIECE_HEIGHT, "sprite", shuffledIndexArray[piecesIndex]);
+                }
+                else { //initial position of black piece
+                    piece = piecesGroup.create(j * PIECE_WIDTH, i * PIECE_HEIGHT);
+                    piece.black = true;
+                }
+                piece.name = 'piece' + i.toString() + 'x' + j.toString();
+                piece.currentIndex = piecesIndex;
+                piece.destIndex = shuffledIndexArray[piecesIndex];
+                piece.inputEnabled = true;
+                piece.events.onInputDown.add(this.selectPiece, this);
+                piece.posX = j;
+                piece.posY = i;
+                piecesIndex++;
+            }
+        }
+    },
+
+    selectPiece: function(piece) {
+
+        var blackPiece = this.canMove(piece);
+
+        //if there is a black piece in neighborhood
+        if (blackPiece) {
+            this.movePiece(piece, blackPiece);
+        }
+
+    },
+
+    canMove: function(piece) {
+
+        var foundBlackElem = false;
+
+        piecesGroup.children.forEach(function(element) {
+            if (element.posX === (piece.posX - 1) && element.posY === piece.posY && element.black ||
+                element.posX === (piece.posX + 1) && element.posY === piece.posY && element.black ||
+                element.posY === (piece.posY - 1) && element.posX === piece.posX && element.black ||
+                element.posY === (piece.posY + 1) && element.posX === piece.posX && element.black) {
+                foundBlackElem = element;
+                return;
+            }
+        });
+        return foundBlackElem;
+    },
+
+    movePiece: function(piece, blackPiece) {
+
+        var tmpPiece = {
+            posX: piece.posX,
+            posY: piece.posY,
+            currentIndex: piece.currentIndex
+        };
+
+        game.add.tween(piece).to({x: blackPiece.posX * PIECE_WIDTH, y: blackPiece.posY * PIECE_HEIGHT}, 300, Phaser.Easing.Linear.None, true);
+
+        //change places of piece and blackPiece
+        piece.posX = blackPiece.posX;
+        piece.posY = blackPiece.posY;
+        piece.currentIndex = blackPiece.currentIndex;
+        piece.name ='piece' + piece.posX.toString() + 'x' + piece.posY.toString();
+
+        //piece is the new black
+        blackPiece.posX = tmpPiece.posX;
+        blackPiece.posY = tmpPiece.posY;
+        blackPiece.currentIndex = tmpPiece.currentIndex;
+        blackPiece.name ='piece' + blackPiece.posX.toString() + 'x' + blackPiece.posY.toString();
+
+        //after every move check if puzzle is completed
+        this.checkIfFinished();
+    },
+
+    checkIfFinished: function() {
+
+        var isFinished = true;
+
+        piecesGroup.children.forEach(function(element) {
+            if (element.currentIndex !== element.destIndex) {
+                isFinished = false;
+                return;
+            }
+        });
+
+        if (isFinished) {
+            this.showFinishedText();
+        }
+
+    },
+
+    showFinishedText: function() {
+
+        heart = game.add.sprite(game.world.centerX, game.world.centerY, 'hearts');
+        heart.animations.add('heart', Phaser.Animation.generateFrameNames('heart',1,7),8,true);
+        heart.anchor.setTo(0.5, 0.5);
+        heart.scale.setTo(2,2);
+
+        alexheart = game.add.sprite(game.world.centerX, game.world.centerY, 'hearts', 'alexheart');
+        alexheart.alpha = 0;
+        alexheart.anchor.setTo(0.5, 0.5);
+        game.add.tween(alexheart).to({alpha: 1}, 2000, Phaser.Easing.Linear.None, true, 1000); 
+
+        heart.animations.play('heart');
+
+        incchat += 1;
+
+        localStorage.setItem("incchat", inchat);
+
+    },
+
+    createShuffledIndexArray: function() {
+
+        var indexArray = [];
+        for (var i = 0; i < piecesAmount; i++)
+        {
+            indexArray.push(i);
+        }
+        return this.shuffle(indexArray);
+    },
+
+    shuffle: function(array) {
+
+        var counter = array.length,
+            temp,
+            index;
+
+        while (counter > 0)
+        {
+            index = Math.floor(Math.random() * counter);
+
+            counter--;
+
+            temp = array[counter];
+            array[counter] = array[index];
+            array[index] = temp;
+        }
+
+        return array;
+        
+    }
+}
---
+++ finalgame/myGame/js/dating/datingProfiles3.js
@@ -0,0 +1,284 @@
+var dateProfiles3 = function(game) {};
+dateProfiles3.prototype = {
+	preload: function() {
+	// ----------------------------------------------------------------
+		
+		// load in site overlay, buttons, and icon borders
+		game.load.image('overlay', 'assets/img/dating/dateoverlay.png');
+		game.load.image('dclick', 'assets/img/dating/detectdate.png');
+		game.load.image('msgbutton', 'assets/img/chat/message.png');
+		game.load.image('iconbrdr', 'assets/img/dating/tintthis.png');
+
+	// ----------------------------------------------------------------
+
+		// Load in main site page
+		game.load.image('datehub', 'assets/img/dating/hub.png');
+
+	// ----------------------------------------------------------------
+
+		// Load in all dating profiles
+		game.load.image('ed', 'assets/img/dating/dateed.png');
+		game.load.image('dhani', 'assets/img/dating/datedhani.png');
+		game.load.image('alex', 'assets/img/dating/datealex.png');
+		game.load.image('yash', 'assets/img/dating/dateyash.png');
+
+	// ----------------------------------------------------------------
+
+	},
+	create: function() {
+		this.camera.flash('#000000', 700);
+		
+		bgmusic.stop();
+		game.stage.backgroundColor = "#000";
+
+	// ----------------------------------------------------------------
+
+		ed = game.add.sprite(0,0,'ed');
+		ed.alpha = 0;
+		dhani = game.add.sprite(0,0,'dhani');
+		dhani.alpha = 0;
+		alex = game.add.sprite(0,0,'alex');
+		alex.alpha = 0;
+		yash = game.add.sprite(0,0,'yash');
+		yash.alpha = 0;
+
+		overlay = game.add.sprite(0,0,'overlay');
+
+		hub = game.add.sprite(0,0,'datehub');
+		// hub.alpha = 1;
+
+	// ----------------------------------------------------------------
+
+		// internal button group to detect if player clicks on icons
+		// or the message button
+		detectEd = game.add.sprite(216, 30, 'dclick');
+		detectEd.alpha = 0;
+		detectEd.inputEnabled = true;
+
+		detectAlex = game.add.sprite(325, 30, 'dclick');
+		detectAlex.alpha = 0;
+		detectAlex.inputEnabled = true;
+		
+		detectDhani = game.add.sprite(432, 30, 'dclick');
+		detectDhani.alpha = 0;
+		detectDhani.inputEnabled = true;
+		
+		detectYash = game.add.sprite(535, 30, 'dclick');
+		detectYash.alpha = 0;
+		detectYash.inputEnabled = true;
+
+	// ----------------------------------------------------------------
+
+		// icon borders -- they are white so I can tint them
+		// they'll be tinted black initially, and then turn to white when 
+		// the internal button is pressed.
+
+		borderEd = game.add.sprite(216, 30, 'iconbrdr');
+		borderEd.tint = 0x000000;
+		borderAlex = game.add.sprite(324, 30, 'iconbrdr');
+		borderAlex.tint = 0x000000;
+		borderDhani = game.add.sprite(432, 30, 'iconbrdr');
+		borderDhani.tint = 0x000000;
+		borderYash = game.add.sprite(534, 30, 'iconbrdr');
+		borderYash.tint = 0x000000;
+
+
+		// callback functions for browsing
+		// click and hover detection
+
+		detectEd.events.onInputDown.add(this.chooseEd, this);
+		detectEd.events.onInputOver.add(this.hoverEd, this);
+
+		detectAlex.events.onInputDown.add(this.chooseAlex, this);
+		detectAlex.events.onInputOver.add(this.hoverAlex, this);
+
+		detectDhani.events.onInputDown.add(this.chooseDhani, this)
+		detectDhani.events.onInputOver.add(this.hoverDhani, this);
+
+		detectYash.events.onInputDown.add(this.chooseYash, this);
+		detectYash.events.onInputOver.add(this.hoverYash, this);
+
+
+	// ----------------------------------------------------------------
+
+	},
+	update: function() {
+
+		// --------------------------------------------------
+		// Press SPACE for main menu
+		// --------------------------------------------------
+		if(game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) {
+			game.state.start('phase3');
+			game.sound.stopAll();
+			if (((localStorage.getItem("incb3") >= 2) && 
+				(localStorage.getItem("incsh3") >= 2) && 
+				(localStorage.getItem("inctr3") >= 1)))
+			{
+				game.sound.stopAll();
+				game.state.start('phase4');
+			} 
+
+		}
+	},
+
+	chooseEd: function(){
+		borderEd.tint = 0xffffff;
+		borderDhani.tint = 0x000000;
+		borderYash.tint = 0x000000;
+		borderAlex.tint = 0x000000;
+
+		ed.alpha = 1;
+		alex.alpha = 0;
+		dhani.alpha = 0;
+		yash.alpha = 0;
+		hub.alpha = 0;
+
+		msgEd = game.add.sprite(660, 36, 'msgbutton');
+		msgEd.alpha = 1;
+		msgEd.inputEnabled = true;
+
+		msgEd.events.onInputOver.add(this.hovedchat, this);
+		msgEd.events.onInputOut.add(this.nohovedchat, this);
+		msgEd.events.onInputDown.add(this.chated, this);
+	},
+
+
+	hoverEd: function() {
+		detectEd.input.useHandCursor = true;
+	},
+
+	hovedchat: function() {
+		msgEd.input.useHandCursor = true;
+		msgEd.tint = 0x88FFCE;
+	},
+
+	nohovedchat: function() {
+		msgEd.tint = 0xffffff;
+	},
+
+	chated: function() {
+		game.state.start('edchat3');
+	},
+	
+	//---------------------------------------------------------------
+
+	chooseAlex: function() {
+		borderAlex.tint = 0xffffff;
+		borderEd.tint = 0x000000;
+		borderDhani.tint = 0x000000;
+		borderYash.tint = 0x000000;
+
+		alex.alpha = 1;
+		dhani.alpha = 0;
+		yash.alpha = 0;
+		ed.alpha = 0;
+		hub.alpha = 0;
+
+		msgAlex = game.add.sprite(660, 36, 'msgbutton');
+		msgAlex.alpha = 1;
+		msgAlex.inputEnabled = true;
+
+		msgAlex.events.onInputOver.add(this.hovalexchat, this);
+		msgAlex.events.onInputOut.add(this.nohovalexchat, this);
+		msgAlex.events.onInputDown.add(this.chatalex, this);
+	},
+
+	hoverAlex: function() {
+		detectAlex.input.useHandCursor = true;
+	},
+
+	hovalexchat: function() {
+		msgAlex.input.useHandCursor = true;
+		msgAlex.tint = 0x88FFCE;
+	},
+
+	nohovalexchat: function() {
+		msgAlex.tint = 0xffffff;
+	},
+
+	chatalex: function() {
+		game.state.start('alexchat3');
+	},
+
+
+	//---------------------------------------------------------------
+
+
+	chooseDhani: function() {
+		borderDhani.tint = 0xffffff;
+		borderYash.tint = 0x000000;
+		borderAlex.tint = 0x000000;
+		borderEd.tint = 0x000000;
+
+		dhani.alpha = 1;
+		yash.alpha = 0;
+		ed.alpha = 0;
+		alex.alpha = 0
+		hub.alpha = 0;
+
+		msgDhan = game.add.sprite(660, 36, 'msgbutton');
+		msgDhan.alpha = 1;
+		msgDhan.inputEnabled = true;
+
+		msgDhan.events.onInputOver.add(this.hovdhanchat, this);
+		msgDhan.events.onInputOut.add(this.nohovdhanchat, this);
+		msgDhan.events.onInputDown.add(this.chatdhan, this);
+	},
+
+
+	hoverDhani: function() {
+		detectDhani.input.useHandCursor = true;
+	},
+
+	hovdhanchat: function() {
+		msgDhan.input.useHandCursor = true;
+		msgDhan.tint = 0x88FFCE;
+	},
+
+	nohovdhanchat: function() {
+		msgDhan.tint = 0xffffff;
+	},
+
+	chatdhan: function() {
+		game.state.start('dhanichat3');
+	},
+
+	//---------------------------------------------------------------
+
+	chooseYash: function() {
+		borderYash.tint = 0xffffff;
+		borderAlex.tint = 0x000000;
+		borderEd.tint = 0x000000;
+		borderDhani.tint = 0x000000;
+
+		yash.alpha = 1;
+		ed.alpha = 0;
+		alex.alpha = 0;
+		dhani.alpha = 0;
+		hub.alpha = 0;
+
+		msgYash = game.add.sprite(660, 36, 'msgbutton');
+		msgYash.alpha = 1;
+		msgYash.inputEnabled = true;
+
+		msgYash.events.onInputOver.add(this.hovyashchat, this);
+		msgYash.events.onInputOut.add(this.nohovyashchat, this);
+		msgYash.events.onInputDown.add(this.chatyash, this);
+	},
+
+	hoverYash: function() {
+		detectYash.input.useHandCursor = true;
+	},
+
+	hovyashchat: function() {
+		msgYash.input.useHandCursor = true;
+		msgYash.tint = 0x88FFCE;
+	},
+	nohovyashchat: function() {
+		msgYash.tint = 0xffffff;	
+	},
+
+	chatyash: function() {
+		game.state.start('yashchat3')
+	},
+}
